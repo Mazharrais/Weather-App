76bd3c1a7ed8d9c9008b5bfc0fccfcf7eca3936d
Mazharrais <mazharmajo72@gmail.com>
2023-11-27 18:42:33 +0500

    add my files

--- app.js
+++ app.js
@@ -2,22 +2,75 @@
 
 
 
-var temp = document.getElementById("parent-child");
-console.log(navigator.geolocation.getCurrentPosition(function(location){
-    console.log(location.coords.longitude)
-  var lat = location.coords.latitude;
-  var long = location.coords.longitude;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4b50650dc723fcc4e7550cbe9c019210&units=metric`)
-    .then(function (res){
-        return res.json()
-    })
-    .then(function (data){
-        temp.innerHTML = data.main.feels_like
-        temp.innerHTML = data.main.humidity
-        temp.innerHTML = data.sys.sunrise
-        temp.innerHTML = data.wind.deg
-        temp.innerHTML = data.main.pressure
-        temp.innerHTML = data.sys.id
-        console.log(data)
-    })
-}))
+// var temp = document.getElementById("parent-child");
+// console.log(temp);
+// console.log(navigator.geolocation.getCurrentPosition(function(location){
+//     console.log(location.coords.longitude)
+//   var lat = location.coords.latitude;
+//   var long = location.coords.longitude;
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4b50650dc723fcc4e7550cbe9c019210&units=metric`)
+//     .then(function (res){
+//         return res.json()
+//     })
+//     .then(function (data){  
+//         temp.innerHTML = data.main.feels_like
+//         temp.innerHTML = data.main.humidity
+//         temp.innerHTML = data.sys.sunrise
+//         temp.innerHTML = data.wind.deg
+//         temp.innerHTML = data.main.pressure
+//         temp.innerHTML = data.sys.id
+//         temp.innerHTML = data.main.feels_like
+//         console.log(temp);
+//     })
+// }))
+
+const searchInput = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const image = document.querySelector('.icon');
+
+
+async function getWeather(city) {
+    var res = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4b50650dc723fcc4e7550cbe9c019210&units=metric`)
+   if(res.status == 404){
+    document.querySelector(".error").style.display = "block";
+   } else{
+    document.querySelector(".error").style.display = "none";
+   }
+   
+    var data = await res.json();
+    console.log(data);
+    document.querySelector('.celsius').innerHTML = Math.round(data.main.temp)  + "°c"
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.humiditytyp').innerHTML = Math.round(data.main.humidity) + "%"
+    document.querySelector('.winds').innerHTML = Math.round(data.wind.speed) + "km/h"
+
+if(data.weather[0].main =="Clouds   "){
+     image.src = "images/cloud img 3.png";
+} else if(data.weather[0].main == "Clear"){
+    image.src = "images/clear-sun 2.png";
+}
+//  else if(data.weather[0].main == "Rain"){
+//     image.src = "images/rain-cloud 2.png";
+// }
+else if(data.weather[0].main == "Rain"){
+    image.src = "images/rain.png"
+}
+else if(data.weather[0].main == "Haze"){
+    image.src = "images/haze 2.png"
+}
+else if(data.weather[0].main == "Thunderstrom"){
+    image.src = "images/thunder-strom.png"
+}
+else if(data.weather[0].main == "snow"){
+    image.src = "images/snow pic.png"
+}
+
+
+
+
+}
+ 
+searchBtn.addEventListener('click', () =>{
+
+    getWeather(searchInput.value);
+})
