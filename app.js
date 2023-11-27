5d0d1b93a6e3cf044cd1fe3eb564cacd1c819300
Mazharrais <mazharmajo72@gmail.com>
2023-11-27 20:32:45 +0500

    add my changes

--- app.js
+++ app.js
@@ -74,3 +74,30 @@ searchBtn.addEventListener('click', () =>{
 
     getWeather(searchInput.value);
 })
+
+
+
+
+
+
+// self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//       caches.match(fetchEvent.request).then(res => {
+//         return res || fetch(fetchEvent.request)
+//       })
+//     )
+//   })
+
+
+
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
